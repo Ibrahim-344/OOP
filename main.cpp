33fec40f3ce996300d9f0b92d1a13b38717a1db1
Muhammad Ibrahim <ihsanibrahim344@gmail.com>
2025-05-15 13:32:07 +0500

    auto

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
-#include<iostream>
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <stdexcept>
 using namespace std;
+template<typename T>
+class DataContainer {
+    vector<T> data;
+
+public:
+    void add(T item) { data.push_back(item); }
+
+    T get(size_t index) {
+        if (index >= data.size()) throw out_of_range("Index out of bounds");
+        return data[index];
+    }
+};
 
 int main() {
-    cout << "Hello, World!" << endl;
-    cout << "This is a simple C++ program." << endl;
-    cout << "It demonstrates basic input and output." << endl;
-    cout << "You can modify this code to suit your needs." << endl;
-    cout << "Feel free to add more functionality!" << endl;
+    try {
+        DataContainer<int> container;
+        container.add(42);
+
+        auto printer = [](const auto &value) {
+            cout << "Value: " << value << endl;
+        };
+
+        shared_ptr<int> ptr = make_shared<int>(100);
+        printer(*ptr);
+        printer(container.get(0));
+
+        // This will throw an exception
+        container.get(1);
+    } catch (const exception &e) {
+        cout << "Exception caught: " << e.what() << endl;
+    }
     return 0;
 }
