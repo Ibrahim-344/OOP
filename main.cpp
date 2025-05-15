c43b0a766adfda0c2ec9f76beb3d5c1bcf17b71c
Muhammad Ibrahim <ihsanibrahim344@gmail.com>
2025-05-15 12:59:17 +0500

    auto

--- main.cpp
+++ main.cpp
@@ -1,36 +1,7 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
-class Person {
-private:
-    string name;
-    int age;
-
-public:
-    Person(string n, int a) : name(n), age(a) {
-    }
-
-    void displayInfo() {
-        cout << "Name: " << name << ", Age: " << age << endl;
-    }
-
-    void setName(string n) {
-        name = n;
-    }
-
-    void setAge(int a) {
-        age = a;
-    }
-};
-
 int main() {
-    Person person("John Doe", 25);
-    person.displayInfo();
-
-    person.setName("Jane Doe");
-    person.setAge(30);
-    person.displayInfo();
-
+    cout << "Hello, World!" << endl;
     return 0;
 }
