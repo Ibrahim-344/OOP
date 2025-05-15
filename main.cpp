180413e4380e7de7451bd2d5d574763d0dc5ce98
Muhammad Ibrahim <ihsanibrahim344@gmail.com>
2025-05-15 13:03:55 +0500

    auto

--- main.cpp
+++ main.cpp
@@ -3,8 +3,5 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
-    cout << "This is a simple C++ program." << endl;
-    cout << "It demonstrates basic input and output." << endl;
-    cout << "You can modify this code to suit your needs." << endl;
     return 0;
 }
